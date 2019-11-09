48eccd6e47a40ad1c836c8f942abce6b7ecccc70
Tylerdclark <46057910+Tylerdclark@users.noreply.github.com>
2019-11-09 01:15:08 -0500

    First commit

---
+++ src/DayNineteenSolution.java
@@ -0,0 +1,39 @@
+/*
+ * File:
+ * Author: Tyler Clark
+ * Date: 
+ * Purpose:
+ */
+
+/**
+ * @author somuc
+ *
+ */
+
+import java.io.*;
+import java.util.*;
+
+interface AdvancedArithmetic{
+   int divisorSum(int n);
+}
+class Calculator implements AdvancedArithmetic {
+    public int divisorSum(int n) {
+        return 0;
+    }
+}
+
+class DayNineteenSolution {
+	
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int n = scan.nextInt();
+		scan.close();
+	    
+		
+		AdvancedArithmetic myCalculator = new Calculator(); 
+		int sum = myCalculator.divisorSum(n);
+		System.out.println("I implemented: " + myCalculator.getClass().getInterfaces()[0].getName() );
+	    System.out.println(sum);
+	}
+}
+
