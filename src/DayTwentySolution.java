fef8e672e25bee040bd16bf4b6e9b55e3684bb26
Tylerdclark <46057910+Tylerdclark@users.noreply.github.com>
2019-11-15 00:27:20 -0500

    Update 20 and 21

--- src/DayTwentySolution.java
+++ src/DayTwentySolution.java
@@ -14,8 +14,7 @@ public class DayTwentySolution {
         
         for (int i = 0; i < n-1; i++) 
             for (int j = 0; j < n-i-1; j++) 
-                if (arr[j] > arr[j+1]) 
-                { 
+                if (arr[j] > arr[j+1]){ 
                     numSwaps++;
                     int temp = arr[j]; 
                     arr[j] = arr[j+1]; 
