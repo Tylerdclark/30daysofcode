2105a29b71ce212566fbf7df16c307c34fc77941
Tylerdclark <46057910+Tylerdclark@users.noreply.github.com>
2019-11-20 21:19:47 -0500

    Update 5/upload 6

--- src/DayFiveSolution.java
+++ src/DayFiveSolution.java
@@ -4,24 +4,20 @@ import java.util.Scanner;
  *
  */
 public class DayFiveSolution {
-	
-  
+	    
+    private static final Scanner scanner = new Scanner(System.in);
 
-  public static void main(String[] args) {
-	int doubleAmount = 0;
-    Scanner scan = new Scanner(System.in);
-    System.out.println("Please enter starting DNA");
-    int dna = scan.nextInt();
-    System.out.println("DNA count:" + dna);
-    System.out.println("Please enter how many times you want it to double");
-    int rounds = scan.nextInt();
-    System.out.println("Round count:" + rounds);
-    for (int i = 0; i < rounds; i++) {
-        doubleAmount += dna;
-        System.out.println(doubleAmount);
-      }
-    
-    scan.close();
-  }
-	
+    public static void main(String[] args) {
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+        int i;
+        for(i=1; i<=10; i++)
+    {
+        int result = n * i;
+        System.out.println(n+ " x " +i+ " = " +result);
+    } 
+        scanner.close();
+        
+    }
 }
+
---
+++ src/DaySixSolution.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+/**
+ * @author somuc
+ *
+ */
+public class DaySixSolution {
+	
+	public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+        int n=scan.nextInt();
+        String words[]=new String[n];
+        for(int i=0;i<n;i++)
+        words[i]=scan.next();
+        scan.close();
+        for(int i=0;i<n;i++) {
+            String even="";
+            String odd="";
+            for(int j=0;j<words[i].length();j++){
+            char[] array=words[i].toCharArray();
+            if(j%2!=0)
+            odd+=array[j];
+            else
+            even+=array[j];
+            }
+            System.out.print(even+" "+odd+"\n");
+        }
+    }
+}
