41db29a695cfdc52497c45178d977b199f6741bf
Tylerdclark <46057910+Tylerdclark@users.noreply.github.com>
2019-11-24 23:27:14 -0500

    Update 8 and upload 9

--- src/DayEightSolution.java
+++ src/DayEightSolution.java
@@ -1,7 +1,33 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 /**
  * @author Tylerdclark
  *
  */
 public class DayEightSolution {
-
+	public static void main(String []argh){
+        
+        Map<String, Integer> phoneBook = new HashMap<String,Integer>();
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+            
+        for(int i = 0; i < n; i++) {                
+            String name = in.next();
+            int phone = in.nextInt();
+            // Write code here
+            phoneBook.put(name, phone);
+         }
+        while(in.hasNext()){
+            String s = in.next();
+            // Write code here                
+            if (phoneBook.get(s) == null ) {  
+                System.out.println("Not found");
+            } else {
+                System.out.println(s + "=" + phoneBook.get(s));
+            }
+        }
+        in.close();
+    }
 }
