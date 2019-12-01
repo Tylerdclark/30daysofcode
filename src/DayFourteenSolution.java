9f6c5ac47b1c61c34741ea7cb2b6b9a16723ecbf
Tylerdclark <46057910+Tylerdclark@users.noreply.github.com>
2019-12-01 13:15:50 -0500

    Create the day fourteen solution

---
+++ src/DayFourteenSolution.java
@@ -0,0 +1,39 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ * @author Tylerdclark
+ *
+ */
+class Difference {
+  	private int[] elements;
+  	public int maximumDifference;
+
+	// Add your code here
+    public Difference(int[] a) {
+      this.elements = a;
+      }
+      
+      public void computeDifference() {
+        Arrays.sort(elements);
+        maximumDifference = elements[elements.length-1] - elements[0];
+      }
+}
+
+public class DayFourteenSolution {
+	public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+        sc.close();
+
+        Difference difference = new Difference(a);
+
+        difference.computeDifference();
+
+        System.out.print(difference.maximumDifference);
+	}
+}
