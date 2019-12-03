fe98c97c3bf226021760c9a8f3bf2be8bf59c7c4
Tylerdclark <46057910+Tylerdclark@users.noreply.github.com>
2019-12-03 14:10:46 -0500

    Create Day Sixteen

---
+++ src/DaySixteenSolution.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+/**
+ * @author somuc
+ *
+ */
+public class DaySixteenSolution {
+	public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        String S = in.next();
+        int stringInt;
+        try {
+            
+            stringInt= Integer.parseInt(S);
+            System.out.println(stringInt);
+            
+        } catch (NumberFormatException e) {
+            
+            System.out.println("Bad String");
+        }
+}
+}
