1ac62ec403f2a70f1aaa4fbbd0e599236d134a89
Tyler Clark <Tylerdclark@users.noreply.github.com>
2019-12-20 12:15:12 -0500

    Put in a bunch of conditionals

--- src/DayTwentyTwoSolution.java
+++ src/DayTwentyTwoSolution.java
@@ -7,13 +7,17 @@ public class DayTwentyTwoSolution{
 
 	public static int getHeight(Node root){
 	//Write your code here
-	int leftCount = 0;
-	int rightCount = 0;
-	if(root==null){
-		return 0;
-	}
-	//else{
-	//if(root.left!=null){
-		
-
+		int leftCount = 0;
+		int rightCount = 0;
+		if(root==null){
+			return 0;
+		}
+		else{
+		if(root.left!=null){
+		leftCount = getHeight(root)+1;
+		}
+		if(root.right!=null){
+		rightCount = getHeight(root)+1;
+		}	
+	}	
 }
