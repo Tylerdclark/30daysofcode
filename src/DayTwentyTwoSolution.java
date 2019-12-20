d0b32fdbd992036e905463dfcbc2627853de1d28
Tyler Clark <Tylerdclark@users.noreply.github.com>
2019-12-20 10:41:25 -0500

    Add initial code

--- src/DayTwentyTwoSolution.java
+++ src/DayTwentyTwoSolution.java
@@ -3,6 +3,17 @@
  * @author somuc
  *
  */
-public class DayTwentyTwoSolution {
+public class DayTwentyTwoSolution{
+
+	public static int getHeight(Node root){
+	//Write your code here
+	int leftCount = 0;
+	int rightCount = 0;
+	if(root==null){
+		return 0;
+	}
+	//else{
+	//if(root.left!=null){
+		
 
 }
